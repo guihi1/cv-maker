6fbb9348ce1d12dc13a31e4cf3e166b366692a95
Guilherme <111528193+guihi1@users.noreply.github.com>
2023-05-06 18:14:30 -0300

    Import InfoInput and add functions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,62 @@
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+import React, { Component } from 'react';
+import InfoInput from './components/InfoInput';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      info: {
+        firstName: '',
+        lastName: '',
+        phoneNumber: '',
+        email: '',
+      },
+      cv: false,
+    }
+  }
+
+  handleInfoChange = (data) => {
+    changeInfo = (e) => {
+      this.setState(
+        info: {
+        [data]: e.target.value,
+      }
+      );
+    }
+  }
+
+  viewCV = () => {
+    this.setState({
+      info: this.state.info,
+      cv: true,
+    });
+  }
+
+  backToForm = () => {
+    this.setState({
+      info: this.state.info,
+      cv: false,
+    });
+  }
+
+  render() {
+    const { info, cv } = this.state;
+
+    if (cv) {
+      return (
+        <div>CV</div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <h1>CV maker</h1>
+          <h2>Personal information</h2>
+          <InfoInput />
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
