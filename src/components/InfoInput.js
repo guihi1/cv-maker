fdaecd52d491bf23d49473bc5c4c6343faeeba4c
Guilherme <111528193+guihi1@users.noreply.github.com>
2023-07-05 19:35:38 -0300

    Add info in state

--- src/components/InfoInput.js
+++ src/components/InfoInput.js
@@ -3,29 +3,63 @@ import React, { Component } from 'react'
 class InfoInput extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      info: {
+        firstName: 'Your name',
+        lastName: 'Your last name',
+        phoneNumber: 'Your phone number',
+        email: 'Your email',
+      },
+      edit: false,
+    }
+  }
+
+  handleInfoChange = data => (e) => {
+    this.setState({
+      info: {
+        [data]: e.target.value,
+      },
+    });
   }
 
   render() {
-    return (
-      <div>
+    const { info, edit } = this.state;
+    if (!edit) {
+      return (
         <div>
-          <p id="first-name">{this.props.firstName}</p>
-          <button>Edit</button>
+          <p>{info.firstName}</p>
+          <p>{info.lastName}</p>
+          <p>{info.phoneNumber}</p>
+          <p>{info.email}</p>
         </div>
+      );
+    } else {
+      return (
         <div>
-          <p id="last-name">{this.props.lastName}</p>
-          <button>Edit</button>
+          <div>
+            <label htmlFor="first-name" type="text"></label>
+            <input id="first-name" />
+            <button>Edit</button>
+          </div>
+          <div>
+            <label htmlFor="last-name"></label>
+            <input id="last-name" type="text" />
+            <button>Edit</button>
+          </div>
+          <div>
+            <label htmlFor="email"></label>
+            <input id="email" type="email" />
+            <button>Edit</button>
+          </div>
+          <div>
+            <label htmlFor="phone"></label>
+            <input id="phone" type="number" />
+            <button>Edit</button>
+          </div>
         </div>
-        <div>
-          <p id="email">{this.props.email}</p>
-          <button>Edit</button>
-        </div>
-        <div>
-          <p id="phone">{this.props.phone}</p>
-          <button>Edit</button>
-        </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
