7f1555c2d02a924a006ef874a254ccf69f0e370e
Guilherme <111528193+guihi1@users.noreply.github.com>
2023-07-07 10:18:32 -0300

    Add function to control edit of components

--- src/App.js
+++ src/App.js
@@ -6,43 +6,32 @@ class App extends Component {
     super();
 
     this.state = {
-      cv: false,
+      editInfo: true,
+      editExp: true,
+      editEdu: true,
     }
   }
 
-  viewCV = () => {
-    if (this.state.cv) {
+  changeEditMode = (section) => {
+    if (this.state[section]) {
       this.setState({
-        info: this.state.info,
-        cv: false,
+        [section]: false,
       });
     } else {
       this.setState({
-        info: this.state.info,
-        cv: true,
-      });
+        [section]: true,
+      })
     }
   }
 
   render() {
-    const { cv } = this.state;
-    if (cv) {
-      return (
-        <div>
-          <InfoInput show={cv} />
-        </div>
-      );
-    } else {
-      return (
-        <div className="App">
-          <h2>Personal information</h2>
-          <InfoInput show={cv} />
-          <h2>Experience</h2>
-          <h2>Education</h2>
-          <button onClick={this.viewCV}>View CV</button>
-        </div>
-      );
-    }
+    const { editInfo, editExp, editEdu } = this.state;
+
+    return (
+      <div className="App">
+        <InfoInput edit={editInfo} mode={this.changeEditMode} />
+      </div>
+    );
   }
 }
 
