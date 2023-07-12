87c085897675eee981a3840b1f54bb1691db4a9d
Guilherme <111528193+guihi1@users.noreply.github.com>
2023-07-12 14:12:41 -0300

    Handle exp and edu with separate functions

--- src/App.js
+++ src/App.js
@@ -9,38 +9,45 @@ class App extends Component {
 
     this.state = {
       editInfo: true,
-      eduIndex: [0],
       eduArray: [<EduInput />],
       expArray: [<ExpInput />],
     }
   }
 
-  changeEditMode = (section) => (e) => {
-    if (this.state[section]) {
+  changeEditMode = () => {
+    if (this.state.editInfo) {
       this.setState({
-        [section]: false,
+        editInfo: false,
       });
     } else {
       this.setState({
-        [section]: true,
+        editInfo: true,
       })
     }
   }
 
-  addSection = (section) => {
+  addEduSection = () => {
     this.setState({
-      [section]: this.state[section].concat(<EduInput />),
+      eduArray: this.state.eduArray.concat(<EduInput />),
+    });
+  }
+
+  addExpSection = () => {
+    this.setState({
+      expArray: this.state.expArray.concat(<ExpInput />),
     });
   }
 
   render() {
-    const { editInfo, editExp, editEdu } = this.state;
+    const { editInfo, eduArray, expArray } = this.state;
 
     return (
       <div className="App">
         <InfoInput edit={editInfo} mode={this.changeEditMode} />
-        <button onClick={() => this.addSection('eduArray')}>Add</button>
-        {this.state.eduArray}
+        <button onClick={this.addEduSection}>Add education</button>
+        {eduArray}
+        <button onClick={this.addExpSection}>Add experience</button>
+        {expArray}
       </div>
     );
   }
